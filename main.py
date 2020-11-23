6e18a11d809fb435cbb74396101a187f79cd9bd4
Greg Hawthorne <ghawthorne@me.com>
2020-11-23 07:48:20 +0100

    add list comprehension to all_states

--- main.py
+++ main.py
@@ -15,13 +15,17 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title="Guess the State", prompt="What's another State name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if not guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
+
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        # new_data = pandas.DataFrame(missing_states)
+        # new_data.to_csv("states_to_learn.csv")
+        # break
 
     if answer_state in all_states:
         guessed_states.append(answer_state)
